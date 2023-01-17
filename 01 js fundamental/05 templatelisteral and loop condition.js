22e3769540f42352a1e4028979a690b9bbd33ff7
ankit patidar <patiankit6555@gmail.com>
2023-01-17 09:38:49 +0530

    js 17/01 2

---
+++ 01 js fundamental/05 templatelisteral and loop condition.js
@@ -0,0 +1,29 @@
+// template literals
+
+let age = 23;
+console.log(`ankit is ${age} year old `);
+
+// ===================================================================
+// if and else condition /
+
+// =====================================================
+// for loop
+
+// while loop
+
+// switch case
+switch (ml) {
+  case "tt":
+    console.log("ml1");
+    break;
+  case "ss":
+    console.log("ss");
+    break;
+
+  default:
+    console.log("not a value ml");
+}
+
+// ternary operator
+
+ans >= 10 ? console.log("hello") : console.log("false");
---
+++ 01 js fundamental/07 truty&falsyValue.js
@@ -0,0 +1,14 @@
+// in js there is 5 falsy value
+undefined;
+Nan;
+null;
+0;
+("");
+
+// when we try to convert this value into boolean this all value give us false
+// but in any other case it is true
+
+// ==  check for equality not type
+// === check for equality as well as type of variable
+
+// use of  || &&
