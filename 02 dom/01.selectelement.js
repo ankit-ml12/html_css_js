ac1a126e3f3bdc0575336dc42a82b9cd31822317
ankit patidar <patiankit6555@gmail.com>
2023-01-19 20:32:49 +0530

    19/01 1

---
+++ 02 dom/01.selectelement.js
@@ -0,0 +1,13 @@
+//you can select class id, and element using the below operation
+document.querySelector(".class");
+document.querySelector("#id");
+document.querySelector("element");
+
+// now to select the text related to this id, class and element we use text content
+
+document.querySelector("messege").textContent;
+
+// DoM:-  Document object model
+// structure representaion of html content . allow us to select html element and style and manupulate them
+
+// Dom is not a part of javascript dom and dommethode are the part of web api that we use using js language
