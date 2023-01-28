07cf341d8c26de8ad4c28cd65755f41ac6652699
ankit patidar <patiankit6555@gmail.com>
2023-01-28 17:57:17 +0530

    into map filter reduce

---
+++ 06 arrayMethod/03 extra dom.js
@@ -0,0 +1,9 @@
+// if you want to insert some html code using javascript
+// we simply use string listeral first to create an html element
+//ans then use
+insertAdjacentHTML(position, text);
+//methode
+//where positin are
+"beforebegin", "afterbegin", "beforeend", "afterend";
+
+//innerhtml is contain html code also but textcontent contain only text of html
---
+++ 06 arrayMethod/04mapFilterReduce.js
@@ -0,0 +1,6 @@
+//map and filter methode create a new array for there operation
+//reduce reduces the element into single element
+
+///  map craete a array of same size with applied operation
+/// filter :- filter the element of the arr and store them in new array according to applied condition
+///reduce := reduce into one element
