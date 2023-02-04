574fa04472945f322ce8fd75a15e74c85c920e38
ankit patidar <patiankit6555@gmail.com>
2023-02-04 13:14:54 +0530

    event prop

---
+++ 08 advance dom/5. eventPropogat.js
@@ -0,0 +1,5 @@
+//whenever a event happen it goes from bubbling and capturing phase
+//in capturing it start from the document and move to target
+//in bubbling it return to the document
+
+//not happen with every event but mostly it happen
