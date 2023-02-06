441bb00a22632d2e19479d8406bc8cc86591713e
ankit patidar <patiankit6555@gmail.com>
2023-02-06 13:33:10 +0530

    fad nab

---
+++ 08 advance dom/8-faddingNav.js
@@ -0,0 +1,22 @@
+///////////////////////////////////////
+// Menu fade animation
+const nav = document.querySelector(".nav");
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+///////////////////////////////////////
