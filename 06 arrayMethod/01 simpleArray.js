b53e909aeb540200eee4c3390d479826b3e6f932
ankit-ml12 <95223229+ankit-ml12@users.noreply.github.com>
2023-04-15 21:48:36 +0530

    Update 01 simpleArray.js

--- 06 arrayMethod/01 simpleArray.js
+++ 06 arrayMethod/01 simpleArray.js
@@ -1,9 +1,9 @@
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-// array are basically object  then have access of methodes
+// array are basically object  then have access of method
 
-//1. slice methode (allready converd)
+//1. slice method (allready converd)
 
-//2. Splice methode
+//2. Splice method
 
 //it modified the actual array
 //in simple word return the same thing that slice methode do
@@ -13,17 +13,17 @@ const ml = movements.splice(3, 2);
 console.log(ml); // 3000 -650
 console.log(movements); //200 450 -400 -130 70 1300
 
-//3. reverse methode
+//3. reverse method
 // it actually change the original array
 movements.reverse();
 console.log(movements);
 
-//4. concate methode allready read
+//4. concate method allready read
 
-//5. join methode
+//5. join method
 //join the every element of array inthe string
 
-//6. at methode
+//6. at method
 const move = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 console.log(move.at(0)); //200
