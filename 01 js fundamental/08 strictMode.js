45ff00b6c0fe100bc3b419b49fb290522ec2a80c
ankit-ml12 <95223229+ankit-ml12@users.noreply.github.com>
2023-04-24 22:38:28 +0530

    Update 08 strictMode.js

--- 01 js fundamental/08 strictMode.js
+++ 01 js fundamental/08 strictMode.js
@@ -3,5 +3,5 @@
 // first :- it do not allowed to do certain things
 // second:- create visible error in cosole where js fail silently
 
-// if certain variable is not initilise then js strict mode give us error in cosole but without script mode we don't get any error
+// if certain variable is not initilise then js strict mode give us error in console but without script mode we don't get any error
 // strict mode also give error when we use reserve words
