cf2f80540a451d8307b298efd05e91131d9246b8
ankit-ml12 <95223229+ankit-ml12@users.noreply.github.com>
2023-04-26 20:36:39 +0530

    Update 02 event.js

--- 02 dom/02 event.js
+++ 02 dom/02 event.js
@@ -1,10 +1,10 @@
 // EventListener
 
-"event is something that happen on the page and with the help of evenlistener we can wait to certain even happen and then react on it";
+"event is something that happen on the page and with the help of eventlistener we can wait to certain event happen and then react on it";
 
-// HOW TO USE EVEN LISTENER
+// HOW TO USE EVENT LISTENER
 document.querySelector(".check").addEventListener("click", function () {
   console.log(document.querySelector(".guess").value);
 });
 //inside event listener we pass one is even and the section the event that is going to happen once the even is occure
-// when we store the value inside console log the value it is allways inthe form of string
+// when we store the value inside console log the value it is allways in the form of string
