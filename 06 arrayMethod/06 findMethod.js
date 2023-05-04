d31e9d50a5fdefe384b4d5e222e727aa6715b2d5
ankit-ml12 <95223229+ankit-ml12@users.noreply.github.com>
2023-05-04 13:56:43 +0530

    Update 06 findMethod.js

--- 06 arrayMethod/06 findMethod.js
+++ 06 arrayMethod/06 findMethod.js
@@ -1,8 +1,8 @@
 let arr = [10, -20, 30, -25, 15, -5];
 
 //find method does not return the new array
-// instead it return the first elemtn which satifie the condition
-//other wise it return undined
+// instead it return the first element which fullfill the condition
+//other wise it return undefine
 
 let ans = arr.find((ele) => ele > 0);
 console.log(ans); //10
